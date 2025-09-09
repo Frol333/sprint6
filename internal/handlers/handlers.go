46b9276730a42c7cd8fec6b1117e77761b548c55
Nikita Frolov <fro1.009@yandex.ru>
2025-09-09 11:49:44 +0300

    first commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,66 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/sprint6/internal/service"
+)
+
+// IndexHandler обрабатывает запросы к корневому эндпоинту и возвращает HTML из файла index.html.
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+// UploadHandler обрабатывает загрузку файла и конвертацию текста/морзе.
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseMultipartForm(10 << 20) // 10 MB limit
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	converted, err := service.DetectAndConvert(string(fileBytes))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	filename := time.Now().UTC().String() + filepath.Ext(handler.Filename)
+	outFile, err := os.Create(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer outFile.Close()
+
+	_, err = outFile.WriteString(converted)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, converted)
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,45 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final-tpl/internal/handlers"
+)
+
+// Server структура, содержащая логгер и http-сервер.
+type Server struct {
+	Logger     *log.Logger
+	HTTPServer *http.Server
+}
+
+// NewServer создает новый HTTP-сервер с заданным логгером и зарегистрированными обработчиками.
+func NewServer(logger *log.Logger) *Server {
+	router := http.NewServeMux()
+
+	// Регистрация обработчиков
+	router.HandleFunc("/", handlers.IndexHandler)
+	router.HandleFunc("/upload", handlers.UploadHandler)
+
+	// Создание экземпляра http.Server
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		Logger:     logger,
+		HTTPServer: httpServer,
+	}
+}
+
+// Start запускает HTTP-сервер.
+func (s *Server) Start() error {
+	s.Logger.Println("Server is starting on port 8080")
+	return s.HTTPServer.ListenAndServe()
+}
